f49dbfa90ec1cb4fa4e922f4a56826e0bbcfc2cb
jessabean <jess@eldredge.io>
2022-10-13 10:18:23 -0400

    Create password context and update slider component

--- src/App.js
+++ src/App.js
@@ -5,36 +5,56 @@ import Slider from './components/Slider/Slider.js';
 import PasswordControls from './components/PasswordControls/PasswordControls.js';
 import StrengthIndicator from './components/StrengthIndicator/StrengthIndicator';
 import Button from './components/Button/Button';
+import PasswordContext from './contexts/Password/PasswordContext';
 
 function App() {
   const DEFAULT_LENGTH = 10;
+  const DEFAULT_PATTERN = 'a';
   const [passwordLength, setPasswordLength] = useState(DEFAULT_LENGTH);
-  const [passwordPattern, setPasswordPattern] = useState('a');
-
-  const handleUpdate = num => {
-    setPasswordLength(parseInt(num));
-  };
+  const [passwordPattern, setPasswordPattern] = useState(DEFAULT_PATTERN);
 
   function handleOptions(options) {
     const pattern = options.join('');
     setPasswordPattern(pattern);
   }
 
+  const updateLength = event => {
+    const num = event.target.value;
+    setPasswordLength(parseInt(num));
+  };
+
+  const updatePattern = array =>  {
+    const pattern = array.join('');
+    setPasswordPattern(pattern);
+  }
+
+  const passwordSettings = {
+    length: passwordLength,
+    pattern: passwordPattern,
+    updateLength,
+    updatePattern,
+  };
+
+
+
+
   return (
-    <div className='wrap'>
-      <header className='app-header'>
-        <h1 className='app-title'>Password Generator</h1>
-      </header>
-      <div className='password-box'>
-        <PasswordInput passwordLength={passwordLength} passwordPattern={passwordPattern}></PasswordInput>
+    <PasswordContext.Provider value={passwordSettings}>
+      <div className='wrap'>
+        <header className='app-header'>
+          <h1 className='app-title'>Password Generator</h1>
+        </header>
+        <div className='password-box'>
+          <PasswordInput passwordLength={passwordLength} passwordPattern={passwordPattern}></PasswordInput>
+        </div>
+        <div className='password-controls'>
+          <Slider></Slider>
+          <PasswordControls handleOptions={handleOptions}></PasswordControls>
+          <StrengthIndicator options={passwordPattern.length}></StrengthIndicator>
+          <Button text='Generate' icon='true'></Button>
+        </div>
       </div>
-      <div className='password-controls'>
-        <Slider handleUpdate={handleUpdate} defaultValue={DEFAULT_LENGTH}></Slider>
-        <PasswordControls handleOptions={handleOptions}></PasswordControls>
-        <StrengthIndicator options={passwordPattern.length}></StrengthIndicator>
-        <Button text='Generate' icon='true'></Button>
-      </div>
-    </div>
+    </PasswordContext.Provider>
   );
 }
 
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,31 +1,24 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import PasswordContext from '../../contexts/Password/PasswordContext';
 import './Slider.css';
 
-function Slider({handleUpdate, defaultValue}) {
+function Slider() {
+  const context = useContext(PasswordContext);
   const MIN_VALUE = 0;
   const MAX_VALUE = 20;
-  const [sliderValue, setSliderValue] = useState(defaultValue);
-
-  function handleValueChange(event) {
-    setSliderValue(event.target.value);
-    handleUpdate(event.target.value);
-  }
 
   return(
-    <>
       <div className='slider'>
         <label className='slider-label'>Character length</label>
-        <span className='slider-value'>{sliderValue}</span>
+        <span className='slider-value'>{context.length}</span>
         <input 
           className='slider-input' 
           type='range' 
           min={MIN_VALUE} 
           max={MAX_VALUE} 
-          value={sliderValue} 
-          onChange={handleValueChange} />
+          value={context.length} 
+          onChange={context.updateLength} />
       </div>
-    </>
   )
 }
-
 export default Slider;
