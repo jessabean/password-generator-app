67893f390d7dcdf1732a7c02a31e0a27e530c956
jessabean <jess@eldredge.io>
2022-12-22 15:39:36 -0500

    so long, context

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Slider from './components/Slider/Slider.js';
 import PasswordControls from './components/PasswordControls/PasswordControls.js';
 import StrengthIndicator from './components/StrengthIndicator/StrengthIndicator';
 import Button from './components/Button/Button';
-import PasswordContextProvider from './contexts/Password/PasswordContextProvider';
 
 function App() {
   const DEFAULT_LENGTH = 10;
@@ -51,7 +50,6 @@ function App() {
         <h1 className='app-title'>Password Generator</h1>
       </header>
 
-      <PasswordContextProvider>
         <div className='password-box'>
           <PasswordInput data={passwordData}></PasswordInput>
         </div>
@@ -61,7 +59,6 @@ function App() {
           <StrengthIndicator patternData={passwordData.pattern}></StrengthIndicator>
           <Button text='Generate' icon='true' isDisabled={!passwordData.valid} onClick={() => generatePassword(passwordData.pattern, passwordData.length)}></Button>
         </div>
-      </PasswordContextProvider>
     </div>
   )
 }
--- src/contexts/Password/PasswordContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const PasswordContext = createContext();
-
-export default PasswordContext;
